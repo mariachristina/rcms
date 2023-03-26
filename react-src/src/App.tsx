53dc84ef97990f24fb60144d9386fe113617ce9e
Maria-Christina-Chatz <mhatziathanasioy@gmail.com>
2023-03-26 14:53:02 +0300

    new template with typescript

--- react-src/src/App.tsx
+++ react-src/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>react-src/src/App.js</code> and save to reload.
+                    Edit <code>react-src/src/App.tsx</code> and save to reload.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
